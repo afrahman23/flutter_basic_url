59e44a1a2af2c1ef6763670341b6a8ff8cc53ed6
Assaf Rahman <afrahman@Assafs-MacBook-Pro.local>
2025-02-11 12:10:20 +0700

    run ok

--- README.md
+++ README.md
@@ -15,3 +15,58 @@ For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
 # flutter_basic_url
+
+`AndroidManifest.xml`
+```xml
+<queries>
+        <!-- If your app checks for SMS support -->
+        <intent>
+            <action android:name="android.intent.action.VIEW" />
+            <data android:scheme="sms" />
+        </intent>
+        <!-- If your app checks for call support -->
+        <intent>
+            <action android:name="android.intent.action.VIEW" />
+            <data android:scheme="tel" />
+        </intent>
+        <!-- If your application checks for inAppBrowserView launch mode support -->
+        <intent>
+            <action android:name="android.support.customtabs.action.CustomTabsService" />
+        </intent>
+    </queries>
+```
+
+
+## 1
+`pubspec.yaml`
+```yaml
+dependencies:
+flutter:
+sdk: flutter
+url_launcher: ^6.0.20
+```
+`gradle-wrapper.properties`
+```properties
+distributionUrl=https\://services.gradle.org/distributions/gradle-8.10-all.zip
+```
+
+## 2
+`debug`
+```debug
+What went wrong:
+FAILURE: Build failed with an exception. * What went wrong: Execution failed for task ':url_launcher_android:compileDebugJavaWithJavac'. > Could not resolve all files for configuration ':url_launcher_android:androidJdkImage'. > Failed to transform core-for-system-modules.jar to match attributes {artifactType=_internal_android_jdk_image, org.gradle.libraryelements=jar, org.gradle.usage=java-runtime}. > Execution failed for JdkImageTransform: /Users/afrahman/Library/Android/sdk/platforms/android-34/core-for-system-modules.jar. > Error while executing process /Users/afrahman/Library/Java/JavaVirtualMachines/openjdk-23.0.2/Contents/Home/bin/jlink with arguments {--module-path /Users/afrahman/.gradle/caches/8.10/transforms/075e04261d13090900c86f63a6a0a19f-d606c5ae-af73-4fd8-b51e-f9330f4e5d74/transformed/output/temp/jmod --add-modules java.base --output /Users/afrahman/.gradle/caches/8.10/transforms/075e04261d13090900c86f63a6a0a19f-d606c5ae-af73-4fd8-b51e-f9330f4e5d74/transformed/output/jdkImage --disable-plugin system-modules}
+``` 
+
+solutoins:
+`setting.gradle`
+```gradle
+plugins {
+..
+id "com.android.application" version "8.3.2" apply false
+..
+}
+```
+
+
+## References:
+- https://www.youtube.com/watch?v=Wthmab2pI-o
---
+++ lib/main.dart
@@ -0,0 +1,135 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/link.dart';
+import 'package:url_launcher/url_launcher.dart';
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'URL Launcher',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'URL Launcher'),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+  bool _hasCallSupport = false;
+  Future<void>? _launched;
+  String _phone = '';
+  @override
+  void initState() {
+    super.initState();
+    // Check for phone call support.
+    canLaunchUrl(Uri(scheme: 'tel', )).then((bool result) {
+      setState(() {
+        _hasCallSupport = result;
+      });
+    });
+  }
+  Future<void> _launchInBrowser(Uri url) async {
+    if (!await launchUrl(
+      url,
+      mode: LaunchMode.externalApplication,
+    )) {
+      throw Exception('Could not launch $url');
+    }
+  }
+  Future<void> _launchInWebView(Uri url) async {
+    if (!await launchUrl(url, mode: LaunchMode.inAppWebView)) {
+      throw Exception('Could not launch $url');
+    }
+  }
+
+  Widget _launchStatus(BuildContext context, AsyncSnapshot<void> snapshot) {
+    if (snapshot.hasError) {
+      return Text('Error: ${snapshot.error}');
+    } else {
+      return const Text('');
+    }
+  }
+  Future<void> _makePhoneCall(String phoneNumber) async {
+    final Uri launchUri = Uri(
+      scheme: 'tel',
+      path: phoneNumber,
+    );
+    await launchUrl(launchUri);
+  }
+  @override
+  Widget build(BuildContext context) {
+    // onPressed calls using this URL are not gated on a 'canLaunch' check
+    // because the assumption is that every device can launch a web URL.
+    final Uri toLaunch =
+    Uri(scheme: 'https', host: 'www.cybrosys.com',);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                    onChanged: (String text) => _phone = text,
+                    decoration: const InputDecoration(
+                        hintText: 'Input the phone number to launch')),
+              ),
+              ElevatedButton(
+                onPressed: _hasCallSupport
+                    ? () => setState(() {
+                  _launched = _makePhoneCall(_phone);
+                })
+                    : null,
+                child: _hasCallSupport
+                    ? const Text('Make phone call')
+                    : const Text('Calling not supported'),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(toLaunch.toString()),
+              ),
+              ElevatedButton(
+                onPressed: () => setState(() {
+                  _launched = _launchInBrowser(toLaunch);
+                }),
+                child: const Text('Launch in browser'),
+              ),
+              const Padding(padding: EdgeInsets.all(16.0)),
+              const Padding(padding: EdgeInsets.all(16.0)),
+              const Padding(padding: EdgeInsets.all(16.0)),
+              const Padding(padding: EdgeInsets.all(16.0)),
+              Link(
+                uri: Uri.parse(
+                    'https://www.cybrosys.com'),
+                target: LinkTarget.blank,
+                builder: (BuildContext ctx, FollowLink? openLink) {
+                  return TextButton.icon(
+                    onPressed: openLink,
+                    label: const Text('Link Widget documentation'),
+                    icon: const Icon(Icons.read_more),
+                  );
+                },
+              ),
+              const Padding(padding: EdgeInsets.all(16.0)),
+              FutureBuilder<void>(future: _launched, builder: _launchStatus),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
